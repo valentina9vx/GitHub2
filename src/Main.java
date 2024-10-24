dbe8c4cf3cfabefb0873fec5fc55f37fae01df59
valentina9vx <valentina.messnerx@gmail.com>
2024-10-24 12:33:29 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 1) Dreierpotenz
+        //System.out.println(istPotenzVonDrei(27));
+
+        // 2) Morsecode
+        ausgabe(3,"");
+
+        // 3) Fibonacci-Zahlen
+        //for (int i = 0; fibonacciZahl(i) < 100; i++) {
+           // System.out.println(fibonacciZahl(i));
+        //}
+    }
+
+  // 1) Potenz von 3
+  public static boolean istPotenzVonDrei(int zahl){
+        if (zahl == 1){
+            return true;
+        }
+        if (zahl % 3 != 0 || zahl <= 0){
+            return false;
+        }
+        return(istPotenzVonDrei(zahl/3));
+  }
+
+  // 2) Morsecode
+  private static void ausgabe(int n, String zeichenkette){
+        if (n==0){
+            System.out.println(zeichenkette);
+        } else{
+            ausgabe(n-1,zeichenkette + ".");
+            if (n>1){
+                ausgabe(n-2,zeichenkette+"-");
+            }
+        }
+  }
+
+  // 3) Fibonacci-Zahlen
+  public static int fibonacciZahl(int n){
+        if (n<=1){
+            return n;
+        }
+        return fibonacciZahl(n-1)+fibonacciZahl(n-2);
+
+
+
+  }
+
+  // 4) Suche mit Dichotomie
+
+
+    }
